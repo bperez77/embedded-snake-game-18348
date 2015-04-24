57a1c2c290c71f96750132c2e79408989d9ed23e
Brandon Perez <bmperez@andrew.cmu.edu>
2015-04-24 17:02:24 -0400

    Added the equivalent version of the stdbool and stdint header files for the MC9S12C128 module.

---
+++ stdbool.h
@@ -0,0 +1,25 @@
+/* stdbool.h
+ *
+ * ECE348, Spring 2015
+ * Group C4
+ * Brandon Perez bmperez
+ * Rohit Banerjee rohitban
+ *
+ * Target: MC9S12C128
+ * CodeWarrior Version: 5.1
+ *
+ * Created: Fri 24 Apr 2015 04:58:04 PM EDT
+ * Last Modified: TODO: Update
+ */
+
+#ifndef STDBOOL_H_
+#define STDBOOL_H_
+
+// Values of true and false
+#define true        1
+#define false       0
+
+// Boolean is the smallest possible size
+typedef char bool;
+
+#endif /* STDBOOL_H_ */
---
+++ stdint.h
@@ -0,0 +1,23 @@
+/* stdint.h
+ *
+ * ECE348, Spring 2015
+ * Group C4
+ * Brandon Perez bmperez
+ * Rohit Banerjee rohitban
+ *
+ * Target: MC9S12C128
+ * CodeWarrior Version: 5.1
+ *
+ * Created: Fri 24 Apr 2015 05:00:24 PM EDT
+ * Last Modified: TODO: Update
+ */
+
+#ifndef STDINT_H_
+#define STDINT_H_
+
+// Defintions for the fundamental sizes
+typedef char uint8_t;
+typedef int uint16_t;
+typedef long uint32_t;
+
+#endif /* STDINT_H_ */
